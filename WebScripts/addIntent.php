deee0370e9a2a102fc0ab79f75fd78c638bc4762
Keval Lakhani <kevallakhani95@yahoo.com>
2017-11-24 12:44:32 -0500

    Update addIntent.php

--- WebScripts/addIntent.php
+++ WebScripts/addIntent.php
@@ -1,9 +1,9 @@
 <?php
 header('Content-Type: application/json');
 
-$apiLink = "https://api.api.ai/v1/";
-$intent = 'd0e0ada0-ea80-4811-8829-53801bd59359';//GET intent name
-$apiKey = 'ae96553a8f294f088e44ae094ddb95c0';//$_GET['api'];
+$apiLink = YOUR_API_LINK;
+$intent = INTENT_NAME;//GET intent name
+$apiKey = YOUR_API_KEY;//$_GET['api'];
 $objectName = 'microphone';//GET object name
 $link = $apiLink.'intents/'.$intent.'?v=20150910';
 
